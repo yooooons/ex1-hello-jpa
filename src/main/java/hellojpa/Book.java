f1782f5d7223f0e85407b3c3a9702f743e597ab4
unknown <yooooonnf@gmail.com>
2022-04-16 22:55:54 +0900

    고급 매핑

---
+++ src/main/java/hellojpa/Book.java
@@ -0,0 +1,13 @@
+package hellojpa;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("B")
+
+public class Book extends Item{
+    private String author;
+    private String isbn;
+}
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,13 +17,21 @@ public class JpaMain {
 
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member);
-            em.persist(team);
+
+            Movie movie = new Movie();
+            movie.setDirector("A");
+            movie.setActor("B");
+            movie.setName("바람과 함께 사라지다.");
+            movie.setPrice(10000);
+            em.persist(movie);
+
+            em.flush();
+            em.clear();
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
+
+
             tx.commit();
 
         } catch (Exception e) {
