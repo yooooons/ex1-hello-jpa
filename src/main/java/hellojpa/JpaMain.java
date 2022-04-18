3fa678f1afd5f76ea66898c1fb3468072a2a24d0
unknown <yooooonnf@gmail.com>
2022-04-18 23:33:16 +0900

    고급 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -18,31 +19,15 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
-            movie.setDirector("A");
-            movie.setActor("B");
-            movie.setName("바람과 함께 사라지다.");
-            movie.setPrice(10000);
-            em.persist(movie);
+            Member member = new Member();
+            member.setUsername("userA");
+            member.setCreatedBy("Kim");
+            member.setCreatedDate(LocalDateTime.now());
 
-            Album album = new Album();
-            album.setArtist("빅뱅");
-            album.setName("봄 여름 가을 겨울");
-            album.setPrice(9000);
-            em.persist(album);
-
-            Movie movie1 = new Movie();
-            movie1.setDirector("C");
-            movie1.setActor("D");
-            movie1.setName("드래곤볼.");
-            movie1.setPrice(10000);
-            em.persist(movie1);
+            em.persist(member);
 
             em.flush();
             em.clear();
-
-            Item item = em.find(Item.class, movie1.getId());
-            System.out.println("item = " + item);
 
 
             tx.commit();
